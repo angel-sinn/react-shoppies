911d3ef53a53d2eec830be6d5d8c82c2ff33ce92
Angel <angelkysinn@gmail.com>
2021-05-07 16:12:51 -0700

    Add text button component

--- src/components/AnimatedButton.js
+++ src/components/AnimatedButton.js
@@ -4,7 +4,7 @@ import { Button, Icon } from 'semantic-ui-react'
 export default function AnimatedButton(props) {
 
   return (
-    <Button animated='vertical' secondary floated={props.floated} active={props.active} disabled={props.disabled} onClick={props.onClick}>
+    <Button animated='vertical' color='black' floated={props.floated} active={props.active} disabled={props.disabled} onClick={props.onClick}>
       <Button.Content visible>{props.children}</Button.Content>
       <Button.Content hidden>
         <Icon name={props.children === 'Remove' ? 'trash alternate' : 'trophy'} />
---
+++ src/components/TextButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+export default function AnimatedButton(props) {
+
+  return (
+    <Button basic inverted color={props.color} onClick={props.onClick}>{props.children}</Button>
+  )
+}
